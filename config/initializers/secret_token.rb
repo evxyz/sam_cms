b17528334cabda4c8493eb053a7cd59f8c25e04f
evx001 <eviexeris@yahoo.com>
2012-07-28 00:42:09 -0400

    sam_cms pre scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamCms::Application.config.secret_token = 'cc97b767c97bc5bb47ecaa9be4d107033a75f75d56dea05f4bdf0355453bd8f5ef64ad96c2c9ca2cb48a3a16700bf3b4be947fe053b25f77d0012e6bbd1ef7b6'
