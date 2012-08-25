bf69e32401d7f1cc76a058ede7cb22b00441495a
evx001 <eviexeris@yahoo.com>
2012-08-25 00:14:07 -0400

    DB + App.CSS

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ if defined?(Bundler)
 end
 
 module SamCms
+ 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
