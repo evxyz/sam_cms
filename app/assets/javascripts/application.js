6fec84bb72c64aa336141b47052bafcfc81a0176
evx001 <eviexeris@yahoo.com>
2012-09-21 02:50:29 -0400

    almost but the main heading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,11 @@
 //= require twitter/bootstrap
 //= require_tree .
 $(document).ready(function() { 
-
-
+$("em") .delay (750) .fadeIn(1000);
+$("h2") .slideDown(1500);
     $('img') .delay (750) .fadeIn(1000);
     $('h3') .delay(300) .fadeIn(2000);
-    $('header')  .slideDown(1500);
+    $('.container-fluid') .delay (750) .fadeIn(1000);
     $('h1')  .delay (750) .fadeIn(1000);
     $('div.container-fluid') .delay (750) .fadeIn(1000);
     
